db08334af27023c0699f2f8f8a61816dbf2694d4
gbrielbaiao <jarviplacanal@gmail.com>
2026-03-27 17:29:48 -0400

    Criando a lógica da aplicação e gerando o grafico.

---
+++ script.js
@@ -0,0 +1,59 @@
+function gerar_infomacoes(qtd_itens, qtd_grupos){
+    let itens = [];
+    for (let item = 1; item <= qtd_itens; item++) {
+        itens.push(Math.floor(Math.random() * qtd_grupos) + 1);
+    }
+
+    let data = {
+        grupos: [],
+        tamanho: []
+    }
+    for (let g = 1; g <= qtd_grupos; g++) { data.grupos.push(g) }
+    
+    for (let t = 0; t < data.grupos.length; t++) {
+        data.tamanho.push(itens.filter(item => item === data.grupos[t]).length);
+    }
+
+    return data;
+}
+
+document.getElementById('botao-gerar')
+.addEventListener('click', () => {
+    const area_grafico = document.getElementById('area-grafico');
+    area_grafico.innerHTML = '';
+    area_grafico.innerHTML = '<canvas id="grafico"></canvas>';
+    
+    try {
+        const quantidade_itens = document.getElementById('quantidade-itens').value;
+        const quantidade_grupos = document.getElementById('quantidade-grupos').value; 
+        if (!quantidade_itens || !quantidade_grupos) {
+            throw Error();
+        }
+
+        const data = gerar_infomacoes(quantidade_itens, quantidade_grupos);
+
+        const grafico = document.getElementById('grafico');
+
+        new Chart(grafico, {
+            type: 'bar',
+            data: {
+                labels: data.grupos,
+                datasets: [{
+                    label: 'Tamanho dos grupos',
+                    data: data.tamanho,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    } catch (err) {
+        window.alert('Algo deu errado. Verifique os valores digitados.')
+    }
+
+});
